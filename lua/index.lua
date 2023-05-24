a9f9fa9bfda242ce8c5664b33886d24ca76cf45f
basicoa <2895991648@qq.com>
2023-05-24 23:12:09 +0800

    update

---
+++ lua/index.lua
@@ -0,0 +1,19 @@
+
+function music_mysql()
+    local ini = require("ini")
+    if ini.open("config.ini") then
+        return require("mysql").open(
+                ini.get("mysql","user"),
+                ini.get("mysql","password"),
+                ini.get("mysql","ip"),
+                ini.get("mysql","port"),
+                ini.get("mysql","database")
+        )
+    end
+end
+
+function main(request)
+    request.Template("index.html",{
+        Title="Music",
+    })
+end
